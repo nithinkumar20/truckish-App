acb4efbd8110d81d9ddd9388386bb63906b0301f
Nithin Kumar m <nithinm.015@gmail.com>
2018-07-11 13:20:20 +0530

    json

--- frontend/src/app/component/add-stadium/add-stadium.component.ts
+++ frontend/src/app/component/add-stadium/add-stadium.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit,Output,EventEmitter } from '@angular/core';
 import { Stadium } from '../../models/stadium';
 import { StadiumService } from '../../services/stadium.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-stadium',
@@ -18,13 +19,15 @@ export class AddStadiumComponent implements OnInit {
   img:''
   }
 
-  constructor(public stadiumService:StadiumService) { }
+  constructor(public stadiumService:StadiumService,
+  public routrt:Router) { }
 
   ngOnInit() {
   }
 
   addStadium(stad:Stadium){
     this.stadiumService.addStadium(stad);
+    this.routrt.navigate(['/']);
   }
 
 }
